5eff3602dd3c9f4988f92bf6d5fdf1fbc524a9f9
Clebson Carvalho <clebson2007.farias@gmail.com>
2017-02-06 00:43:03 -0300

    Pandas

--- arquivoTxt.py
+++ arquivoTxt.py
@@ -1,5 +1,8 @@
 import os
 import collections
+import calendar as ca
+import pandas as pd
+import numpy as np
 
 def listaTxt(caminho):
 	listaDir = os.listdir(caminho)
@@ -38,38 +41,17 @@ def trabaLinhas(listaLinhas):
             count += 1
             if count == 3:
                 mes = int(i.split("/")[1])
-                ano = int(i.split("/")[2]) 
-                dias = diasDoMes(mes, ano)
+                ano = int(i.split("/")[2])
+                dias = ca.monthrange(ano, mes)[1]
             if count >= 17 and count < 17+dias:
                 if i != '':
                     vazoes.append(float(i.replace(',','.')))
                 else:
-                    vazoes.append(-9999.9)
+                    vazoes.append(np.NaN)
         
         dicVazoes[linha[2], linha[1]] = vazoes
-    return dicVazoes
+    return pd.Series(dicVazoes, index = dicVazoes.keys())
 
-
-def diasDoMes(mes, ano):
-	"""
-	:param mes:
-	:param ano:
-	:return: Quantidades de dias do mês
-	"""
-	if mes <= 7:
-		if mes == 2:
-			if (ano % 4) == 0 and (ano % 100) != 0 or (ano % 400) == 0:
-				dias = 29
-			else:
-				dias = 28
-		else:
-			if (mes%2) == 0:
-				dias = 30
-			else:
-				dias = 31
-	else:
-		if (mes%2) == 0:
-			dias = 31
-		else:
-			dias = 30
-	return dias
+if __name__ == "__main__":
+    caminho = os.getcwd()
+    print (type(trabaLinhas(lerTxt(caminho, "4933000"))))
