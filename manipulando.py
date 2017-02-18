c808ad7476a9762dc1408edcc0296a2daf7221d7
Clebsonpy <Clebson2007.farias@gmail.com>
2017-02-18 05:42:45 -0300

    Gráfico

--- manipulando.py
+++ manipulando.py
@@ -9,6 +9,33 @@ Created on Mon Jan 30 15:46:00 2017
 import pandas as pd
 import arquivoTxt as arq
 import os
+import calendar
+
+
+def grupoAnoHidro(dados, mesHidro, nPosto, graf=False):
+    dias = calendar.monthrange(2000, mesHidro-1)[1]
+    mes = {1:'JAN', 2:'FEB', 3:'MAR', 4:'APR', 5:'MAY', 6:'JUN', 7:'JUL', 8:'AUG', 9:'SEP', 10:'OCT', 11:'NOV', 12:'DEC'}
+    grupo = dados.groupby(pd.Grouper(freq='A-%s' % mes[mesHidro]))
+    frame = pd.DataFrame(index=pd.date_range(pd.to_datetime('1999/%s/1' % mesHidro), pd.to_datetime('2000/%s/%s' % (mesHidro-1, dias))))
+    for dado in grupo:
+        index = []
+        for data in dado[1].index:
+            if data.month > (mesHidro-1):
+                ano = 1999
+            else:
+                ano = 2000
+            index.append(pd.to_datetime('%s/%s/%s' % (ano, data.month, data.day)))
+    
+        aux = dado[1][nPosto].rename(dado[0].year)
+        frameAux = pd.DataFrame(aux)
+        frameAux.set_index(pd.Index(index), inplace=True)
+        frame = arq.combinaDateFrame(frame, frameAux)
+    if graf:
+        frame.plot()
+        
+    return grupo
+
+
 
 def mesInicioAnoHidrologico(dados):
     grupoMesAno = dados.groupby(pd.Grouper(freq='M')).mean().to_period()
@@ -16,7 +43,7 @@ def mesInicioAnoHidrologico(dados):
     indexN = pd.MultiIndex.from_tuples(indexMult, names=["Mes", "Ano"])
     grupoMesAno.set_index(indexN, inplace=True)
     grupoMes = grupoMesAno.groupby(level='Mes').mean()
-    return grupoMes.idxmin()
+    return int(round(grupoMes.idxmin().mean()))
 
 
 def separaDadosConsisBruto(dados, tipo, lev):
@@ -26,17 +53,15 @@ def separaDadosConsisBruto(dados, tipo, lev):
 
 
 def manipDados(dadosVazao):
-    dadosVazao.sort_index(level='Data', inplace=True)
+    dadosVazao.sort_index(inplace=True)
     nFalhas = dadosVazao.isnull().sum()
-    dadosConsistido = separaDadosConsisBruto(dadosVazao, tipo = 1, lev = 1)
-    dadosBruto = separaDadosConsisBruto(dadosVazao, tipo = 2, lev = 1)
-    ganttConsistido = dadosConsistido.isnull().groupby(pd.Grouper(freq = 'M')).sum().to_period()
-    ganttBruto = dadosBruto.isnull().groupby(pd.Grouper(freq = 'M')).sum()
-    #grupoBruto = dadosBruto.groupby(pd.Grouper(freq = 'A-AUG'))
-    return nFalhas, ganttConsistido, ganttBruto
+    gantt = dadosVazao.isnull().groupby(pd.Grouper(freq = 'M')).sum().to_period()
+
+    return nFalhas, gantt
 
 if __name__ == "__main__":
     caminho = caminho = os.getcwd()
-    dadosVazao = arq.trabaLinhas(caminho)
-    mes = mesInicioAnoHidrologico(separaDadosConsisBruto(dadosVazao, tipo=2,lev=1))
+    dadosVazao = separaDadosConsisBruto(arq.trabaLinhas(caminho), tipo=2,lev=1)
+    mes = mesInicioAnoHidrologico(dadosVazao)
     print(mes)
+    grupo = grupoAnoHidro(dadosVazao, mes, '49330000',graf=True)
