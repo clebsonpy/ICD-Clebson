134bdbb87514974267906b50393eeb2dffe35673
Clebson Carvalho <clebson2007.farias@gmail.com>
2017-02-19 21:27:19 -0300

    Com duvida

--- manipulando.py
+++ manipulando.py
@@ -12,11 +12,16 @@ import os
 import calendar
 
 
-def grupoAnoHidro(dados, mesHidro, nPosto = None):
+
+def grupoAnoHidro(dados, mesHidro, nPosto = None, grafico = False):
     dias = calendar.monthrange(2000, mesHidro-1)[1]
     mes = {1:'JAN', 2:'FEB', 3:'MAR', 4:'APR', 5:'MAY', 6:'JUN', 7:'JUL', 8:'AUG', 9:'SEP', 10:'OCT', 11:'NOV', 12:'DEC'}
-    grupo = dados.groupby(pd.Grouper(freq='A-%s' % mes[mesHidro]))
     if nPosto != None:
+        grupo = dados[nPosto].groupby(pd.Grouper(freq='A-%s' % mes[mesHidro]))
+    else:
+        grupo = dados.groupby(pd.Grouper(freq='A-%s' % mes[mesHidro]))
+        
+    if grafico and nPosto != None:
         frame = pd.DataFrame(index=pd.date_range(pd.to_datetime('1999/%s/1' % mesHidro), pd.to_datetime('2000/%s/%s' % (mesHidro-1, dias))))
         for dado in grupo:
             index = []
@@ -27,15 +32,28 @@ def grupoAnoHidro(dados, mesHidro, nPosto = None):
                     ano = 2000
                 index.append(pd.to_datetime('%s/%s/%s' % (ano, data.month, data.day)))
         
-            aux = dado[1][nPosto].rename(dado[0].year)
+            aux = dado[1].rename(dado[0].year)
             frameAux = pd.DataFrame(aux)
             frameAux.set_index(pd.Index(index), inplace=True)
             frame = arq.combinaDateFrame(frame, frameAux)
         frame.plot(legend=False)
+        return grupo, frame
         
-    return grupo, frame
+    return grupo
 
 
+def maximaAnual(grupos, nPosto):
+    vazaoMax = []
+    dataMax = []
+    for grupo in grupos:
+        vazaoMax.append(grupo[1].max())
+        dataMax.append(grupo[1].idxmax())
+        print(dataMax)
+    maxAnualSeie =  pd.Series(vazaoMax, dataMax, name=nPosto)
+    maxAnual = pd.DataFrame(maxAnualSeie)
+    
+    return maxAnual    
+    
 
 def mesInicioAnoHidrologico(dados):
     grupoMesAno = dados.groupby(pd.Grouper(freq='M')).mean().to_period()
@@ -61,7 +79,10 @@ def manipDados(dadosVazao):
 
 if __name__ == "__main__":
     caminho = caminho = os.getcwd()
-    dadosVazao = separaDadosConsisBruto(arq.trabaLinhas(caminho), tipo=1,lev=1)
+    dadosVazao = separaDadosConsisBruto(arq.trabaLinhas(caminho), tipo=2,lev=1)
     mes = mesInicioAnoHidrologico(dadosVazao)
     print(mes)
-    grupo = grupoAnoHidro(dadosVazao, mes, nPosto='49330000')
+    grupos = grupoAnoHidro(dadosVazao, mes, nPosto='49330000')
+    maxAnual = maximaAnual(grupos, nPosto='49330000')
+    
+    
